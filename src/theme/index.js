c20aec01f99c3785029f6e953e43ffcb99154ab9
Alecsander Marques <alecsander.marques@sooftplan.com.br>
2020-09-12 13:43:20 -0300

    Adicionando Material UI ao projeto e fonte Poppins

---
+++ src/theme/index.js
@@ -0,0 +1,46 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "Poppins",
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        "*": {
+          margin: 0,
+          padding: 0,
+          outline: 0,
+          boxSizing: "border-box",
+        },
+        body: {
+          textRendering: "optimizeLegibility !important",
+          WebkitFontSmoothing: "antialiased !important",
+        },
+        button: {
+          cursor: "pointer",
+        },
+        a: {
+          textDecoration: "none",
+        },
+        "::-webkit-scrollbar": {
+          width: 8,
+        },
+        "::-webkit-scrollbar-track": {
+          background: "#fff",
+        },
+        "::-webkit-scrollbar-thumb": {
+          background: "#ccc",
+        },
+        "::-webkit-scrollbar-thumb:hover": {
+          background: "#bbbbbb",
+        },
+        "::-webkit-scrollbar-thumb:window-inactive": {
+          background: "#ccc",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
